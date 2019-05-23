2e26705181af43b79d91eef289d43f26cfda2445
Simon <simonmarkholland@gmail.com>
2019-05-23 22:53:57 +0700

    Add images and content tags

--- articles/models.py
+++ articles/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 
 
@@ -20,11 +25,26 @@ class ArticleIndexPage(Page):
         context['articlepages'] = articlepages
         return context
 
+class ArticlePageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'ArticlePage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
 
 class ArticlePage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    tags = ClusterTaggableManager(through=ArticlePageTag, blank=True)
+
+
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
@@ -32,7 +52,36 @@ class ArticlePage(Page):
     ]
 
     content_panels = Page.content_panels + [
-        FieldPanel('date'),
+        MultiFieldPanel([
+            FieldPanel('date'),
+            FieldPanel('tags'),
+        ], heading="Article information"),
         FieldPanel('intro'),
-        FieldPanel('body', classname="full"),
+        FieldPanel('body'),
+        InlinePanel('gallery_images', label="Gallery images"),
     ]
+
+class ArticlePageGalleryImage(Orderable):
+    page = ParentalKey(ArticlePage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
+    ]
+
+class ArticleTagIndexPage(Page):
+
+    def get_context(self, request):
+
+        # Filter by tag
+        tag = request.GET.get('tag')
+        articlepages = ArticlePage.objects.filter(tags__name=tag)
+
+        # Update template context
+        context = super().get_context(request)
+        context['articlepages'] = articlepages
+        return context
