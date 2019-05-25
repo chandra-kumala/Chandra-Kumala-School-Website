84b291ec28018b04d7bd31a98b6a0837e788d00d
Simon <simonmarkholland@gmail.com>
2019-05-25 12:34:05 +0700

    Remove unused line.

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.dev")
 
 application = get_wsgi_application()
