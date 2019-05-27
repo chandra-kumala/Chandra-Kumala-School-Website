eef87e2e2a2caa0674e3ded29d9c810f7e86c1bc
Simon <simonmarkholland@gmail.com>
2019-05-27 20:07:26 +0700

    Add images to homepage, in preperation for carousel.

--- home/models.py
+++ home/models.py
@@ -1,12 +1,25 @@
 from django.db import models
-
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 class HomePage(Page):
     body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
+        InlinePanel('related_links', label="Related links"),
+    ]
+
+
+class HomePageRelatedLink(Orderable):
+    page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='related_links')
+    image = models.ForeignKey('wagtailimages.Image',  null=True, blank=True, on_delete=models.CASCADE, related_name='+')
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
     ]
