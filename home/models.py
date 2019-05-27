8bd62a2cb1db27d81c4217c3e889d8880ce13a5d
Simon <simonmarkholland@gmail.com>
2019-05-27 22:36:41 +0700

    Add streamfield and rename fields

--- home/models.py
+++ home/models.py
@@ -1,25 +1,37 @@
 from django.db import models
 from modelcluster.fields import ParentalKey
 from wagtail.core.models import Page, Orderable
-from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.core import blocks
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, StreamFieldPanel
+from wagtail.images.blocks import ImageChooserBlock 
 from wagtail.images.edit_handlers import ImageChooserPanel
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
+    intro = RichTextField(blank=True)
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ], blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
-        InlinePanel('carousel_images', label="Carousel images"),
+        FieldPanel('intro', classname="full"),
+        InlinePanel('carousel_items', label="Carousel images"),
+        StreamFieldPanel('body', classname="full"),
     ]
 
 
 class HomePageRelatedLink(Orderable):
-    page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='carousel_images')
-    image = models.ForeignKey('wagtailimages.Image',  null=True, blank=True, on_delete=models.CASCADE, related_name='+')
+    page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='carousel_items')
+    title = models.CharField(blank=True, max_length=250)
     caption = models.CharField(blank=True, max_length=250)
+    image = models.ForeignKey('wagtailimages.Image',  null=True, blank=True, on_delete=models.CASCADE, related_name='+')
+
 
     panels = [
-        ImageChooserPanel('image'),
+        FieldPanel('title'),
         FieldPanel('caption'),
+        ImageChooserPanel('image'),
+
     ]
