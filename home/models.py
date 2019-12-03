6f9fd554118a06e649c231a7e3588e53bcac4e52
Simon <simonmarkholland@gmail.com>
2019-12-03 15:10:57 +0000

    Replace homepage and carousel

--- home/models.py
+++ home/models.py
@@ -1,37 +1,48 @@
 from django.db import models
 from modelcluster.fields import ParentalKey
 from wagtail.core.models import Page, Orderable
-from wagtail.core.fields import RichTextField, StreamField
-from wagtail.core import blocks
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, StreamFieldPanel
-from wagtail.images.blocks import ImageChooserBlock 
+from wagtail.admin.edit_handlers import InlinePanel, FieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 
-class HomePage(Page):
-    intro = RichTextField(blank=True)
-    body = StreamField([
-        ('heading', blocks.CharBlock(classname="full title")),
-        ('paragraph', blocks.RichTextBlock()),
-        ('image', ImageChooserBlock()),
-    ], blank=True)
+from tools.models import Dreamer, Seo
 
-    content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full"),
+import datetime
+dom = datetime.datetime.now().strftime ("%d")
+
+
+
+class HomePage(Page, Dreamer, Seo):
+
+    def get_context(self, request):
+        context = super(HomePage, self).get_context(request)
+        context['day_of_month'] = dom
+        return context
+
+
+    parent_page_types = ['wagtailcore.page', 'home.HomePage']
+    subpage_types = ['tools.Index', 'home.HomePage']
+
+
+    content_panels = Page.content_panels + Dreamer.panels + [
         InlinePanel('carousel_items', label="Carousel images"),
-        StreamFieldPanel('body', classname="full"),
-    ]
+    ] 
+     
+    promote_panels = Page.promote_panels + Seo.panels
 
 
-class HomePageRelatedLink(Orderable):
+    class Meta:
+        verbose_name = "Home Page"
+        verbose_name_plural = "Home Pages"
+
+
+class Carousel(Orderable):
     page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='carousel_items')
     title = models.CharField(blank=True, max_length=250)
     caption = models.CharField(blank=True, max_length=250)
     image = models.ForeignKey('wagtailimages.Image',  null=True, blank=True, on_delete=models.CASCADE, related_name='+')
 
-
     panels = [
         FieldPanel('title'),
         FieldPanel('caption'),
         ImageChooserPanel('image'),
-
     ]
