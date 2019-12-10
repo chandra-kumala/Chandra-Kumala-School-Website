b9d849d46b3b911d75df5d6dcb9902894d6e54af
Simon <simonmarkholland@gmail.com>
2019-12-10 12:32:49 +0000

    Reformat for clarity

--- home/models.py
+++ home/models.py
@@ -4,36 +4,29 @@ from wagtail.core.models import Page, Orderable
 from wagtail.admin.edit_handlers import InlinePanel, FieldPanel, StreamFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.core.fields import StreamField
-
-
 from tools.models import Dreamer, Seo
-
-import datetime
-dom = datetime.datetime.now().strftime ("%d")
-
 
 
 class HomePage(Page, Dreamer, Seo):
 
     def get_context(self, request):
         context = super(HomePage, self).get_context(request)
-        context['menuitems'] = request.site.root_page.get_descendants(inclusive=True).live().in_menu()
-        # context['menuitems'] = self.get_children().filter(live=True, show_in_menus=True)
-
+        context['menuitems'] = request.site.root_page.get_descendants(
+            inclusive=True).live().in_menu()
 
         return context
 
     parent_page_types = ['wagtailcore.page', 'home.HomePage']
-    subpage_types = ['tools.Index', 'tools.GoogleMaps', 'tools.GoogleCalendar', 'home.HomePage']
+    subpage_types = ['tools.Index', 'tools.GoogleMaps',
+                     'tools.GoogleCalendar', 'home.HomePage']
 
     content_panels = Page.content_panels + [
         StreamFieldPanel('body'),
         InlinePanel('carousel_items', label="Carousel images"),
         StreamFieldPanel('end'),
-    ] 
-     
-    promote_panels = Page.promote_panels + Seo.panels
+    ]
 
+    promote_panels = Page.promote_panels + Seo.panels
 
     class Meta:
         verbose_name = "Home Page"
@@ -41,10 +34,12 @@ class HomePage(Page, Dreamer, Seo):
 
 
 class Carousel(Orderable):
-    page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='carousel_items')
+    page = ParentalKey(HomePage, on_delete=models.CASCADE,
+                       related_name='carousel_items')
     title = models.CharField(blank=True, max_length=250)
     caption = models.CharField(blank=True, max_length=250)
-    image = models.ForeignKey('wagtailimages.Image',  null=True, blank=True, on_delete=models.CASCADE, related_name='+')
+    image = models.ForeignKey('wagtailimages.Image',  null=True,
+                              blank=True, on_delete=models.CASCADE, related_name='+')
 
     panels = [
         FieldPanel('title'),
